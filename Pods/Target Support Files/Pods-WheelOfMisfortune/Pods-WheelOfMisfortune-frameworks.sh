e5f57590e6917c0de796a188e4b1d877ddbb20c1
Matheus Martins <matheusmcrds@gmail.com>
2016-11-07 10:17:13 -0300

    Revert "Added SwiftyJSON"
    This reverts commit 69e6de154bb0213e78e85ae5ea549d0eea66be3c.

--- Pods/Target Support Files/Pods-WheelOfMisfortune/Pods-WheelOfMisfortune-frameworks.sh
+++ Pods/Target Support Files/Pods-WheelOfMisfortune/Pods-WheelOfMisfortune-frameworks.sh
@@ -85,11 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Koloda/Koloda.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Koloda/Koloda.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
