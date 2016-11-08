075389fcb2035b9c91959389d3a473b74f3177d3
Matheus Martins <matheusmcrds@gmail.com>
2016-11-08 12:04:16 -0300

    interface

--- WheelOfMisfortune/Game.swift
+++ WheelOfMisfortune/Game.swift
@@ -9,17 +9,54 @@
 import Foundation
 
 protocol GameDelegate {
-    
+    func game(game: Game, didShowCard card: Card)
 }
 
 class Game {
+    
+    var delegate: GameDelegate?
     
     var moral: Int = 0
     var money: Int = 0
     var order: Int = 0
     var power: Int = 0
     
+    var currentCard: Card? = nil
+    
     func start() {
-
+        showCardAtGroup(group: 0, andIndex: 0)
+    }
+    
+    func showCardAtGroup(group: Int, andIndex index: Int) {
+        self.currentCard = CardsJSON.cardAtGroup(group: group, andIndex: index)
+        self.delegate?.game(game: self, didShowCard: self.currentCard!)
+    }
+    
+    func acceptCurrentCard() {
+        for action in self.currentCard!.acceptActions {
+            self.doCardAction(action: action)
+        }
+    }
+    
+    func declineCurrentCard() {
+        for action in self.currentCard!.declineActions {
+            self.doCardAction(action: action)
+        }
+    }
+    
+    func doCardAction(action: CardAction) {
+        switch action {
+        case let .AddMoral(moral):
+            self.moral += moral
+        case let .AddMoney(money):
+            self.money += money
+        case let .AddOrder(order):
+            self.order += order
+        case let .AddPower(power):
+            self.power += power
+        case let .ShowCard(group, id):
+            self.showCardAtGroup(group: group, andIndex: id)
+        default: break
+        }
     }
 }
--- WheelOfMisfortune/ViewController.swift
+++ WheelOfMisfortune/ViewController.swift
@@ -9,30 +9,53 @@
 import UIKit
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GameDelegate {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var portrait: UIImageView!
+    
+    @IBOutlet weak var moralLabel: UILabel!
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var orderLabel: UILabel!
+    @IBOutlet weak var powerLabel: UILabel!
 
+    @IBOutlet weak var textView: UITextView!
+    
+    var game: Game?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        do {
-            let path = Bundle.main.path(forResource: "Cards", ofType: "json")
+        game = Game()
         
-            let data = try NSData(contentsOf: NSURL(fileURLWithPath: path!) as URL, options: .mappedIfSafe)
-        
-            let json = JSON(data: data as Data)
-            
-            let card = Card(json: json["cards"][0][0])
-            
-            print(card.description)
-        } catch {
-            
-        }
+        game?.delegate = self
+        game?.start()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func game(game: Game, didShowCard card: Card) {
+        
+        self.titleLabel.text = card.title
+        self.textView.text = card.text
+        
+        self.moralLabel.text = game.moral.description
+        self.moneyLabel.text = game.money.description
+        self.orderLabel.text = game.order.description
+        self.powerLabel.text = game.power.description
+    }
+    
+    @IBAction func declineButtonPressed(_ sender: UIButton) {
+        game?.declineCurrentCard()
+    }
+    
+
+    @IBAction func acceptButtonPressed(_ sender: AnyObject) {
+        game?.acceptCurrentCard()
     }
 }
 
