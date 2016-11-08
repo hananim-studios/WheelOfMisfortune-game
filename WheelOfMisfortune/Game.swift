87ffc2d25f61b07a9a321631938985752831a4a1
Matheus Martins <matheusmcrds@gmail.com>
2016-11-08 10:48:22 -0300

    Cards + JSON deserialization

--- WheelOfMisfortune/Game.swift
+++ WheelOfMisfortune/Game.swift
@@ -14,27 +14,12 @@ protocol GameDelegate {
 
 class Game: CardDelegate {
     
-    var cards: [Card] = []
-    
     var moral: Int = 0
     var money: Int = 0
     var order: Int = 0
     var power: Int = 0
     
     func start() {
-        self.cards = getRandomCards(quantity: 5)
-    }
-    
-    func getRandomCards(quantity: Int) -> [Card] {
-        // TODO
-        return []
-    }
-    
-    func didAcceptCard(card: Card) {
-        
-        self.moral += card.moralImpact
-        self.money += card.moneyImpact
-        self.order += card.orderImpact
-        self.power += card.powerImpact
+
     }
 }
--- WheelOfMisfortune/ViewController.swift
+++ WheelOfMisfortune/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            let path = Bundle.main.path(forResource: "Cards", ofType: "json")
+        
+            let data = try NSData(contentsOf: NSURL(fileURLWithPath: path!) as URL, options: .mappedIfSafe)
+        
+            let json = JSON(data: data as Data)
+            
+            let card = Card(json: json["cards"][0][0])
+            
+            print(card.description)
+        } catch {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
