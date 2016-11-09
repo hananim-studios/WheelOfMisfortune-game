b71956113e1aee90b86cd0ba14f4eaacd0097d3f
Matheus Vasconcelos de Sousa <matheusvds@Matheuss-MacBook-Pro.local>
2016-11-09 09:29:35 -0300

    solved conflicts

--- WheelOfMisfortune/ViewController.swift
+++ WheelOfMisfortune/ViewController.swift
@@ -9,13 +9,7 @@
 import UIKit
 import SwiftyJSON
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-class ViewController: UIViewController, WatchConnectionManagerPhoneDelegate {
-=======
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
-class ViewController: UIViewController, GameDelegate {
+class ViewController: UIViewController, GameDelegate, WatchConnectionManagerPhoneDelegate {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var portrait: UIImageView!
@@ -24,10 +18,6 @@ class ViewController: UIViewController, GameDelegate {
     @IBOutlet weak var moneyLabel: UILabel!
     @IBOutlet weak var orderLabel: UILabel!
     @IBOutlet weak var powerLabel: UILabel!
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
 
     @IBOutlet weak var textView: UITextView!
     
@@ -36,21 +26,12 @@ class ViewController: UIViewController, GameDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-<<<<<<< HEAD
-<<<<<<< HEAD
         ConnectionManager.sharedConnectionManager.delegate = self
-=======
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
         
         game = Game()
         
         game?.delegate = self
         game?.start()
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,8 +39,6 @@ class ViewController: UIViewController, GameDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-<<<<<<< HEAD
-<<<<<<< HEAD
     // MARK: WatchConnectivityManagerDelegate
     
     func connectionManager(_ connectionManager: ConnectionManager, updatedWithResponse response: String) {
@@ -67,9 +46,7 @@ class ViewController: UIViewController, GameDelegate {
            //TODO -- atualizar interface
             // response : String representa a resposta recebida do Watch - "0" (Não) ou "1" (Sim)
         })
-=======
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
+        
     func game(game: Game, didShowCard card: Card) {
         
         self.titleLabel.text = card.title
@@ -88,10 +65,6 @@ class ViewController: UIViewController, GameDelegate {
 
     @IBAction func acceptButtonPressed(_ sender: AnyObject) {
         game?.acceptCurrentCard()
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 075389fcb2035b9c91959389d3a473b74f3177d3
     }
 }
 
